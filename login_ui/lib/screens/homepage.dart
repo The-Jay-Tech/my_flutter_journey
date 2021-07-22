c9b93704039a827a13f1ac6c694a94a326be9ff5
Samuel Adekunle <samuelbeebest@gmail.com>
2021-07-22 11:21:56 +0100

    working on the designs

--- login_ui/lib/screens/homepage.dart
+++ login_ui/lib/screens/homepage.dart
@@ -1,10 +1,10 @@
-import 'dart:html' as html;
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:login_ui/screens/sign_in.dart';
 import 'package:login_ui/styles/images.dart';
 import 'package:login_ui/styles/text.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -28,12 +28,12 @@ class _HomePageState extends State<HomePage> {
             child: Card(
               elevation: 1,
               color: Color(0xFF251734).withOpacity(0.3),
-              margin: EdgeInsets.only(left: 32, right: 32),
+              margin: EdgeInsets.only(left: 25, right: 25),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(23),
               ),
               child: Container(
-                padding: EdgeInsets.fromLTRB(12, 16, 12, 32),
+                padding: EdgeInsets.fromLTRB(12, 25, 12, 32),
                 width: 320,
                 decoration: BoxDecoration(
                   color: Color(0xFF251734).withOpacity(0.3),
@@ -45,10 +45,7 @@ class _HomePageState extends State<HomePage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'Safey Road',
-                      style: AppTextStyle().titleStyle,
-                    ),
+                    Text('Safey Road', style: AppTextStyle().titleStyle),
                     Text(
                       'You will find whatever you are looking for',
                       style: AppTextStyle().subTitleStyle,
@@ -69,7 +66,7 @@ class _HomePageState extends State<HomePage> {
                         borderRadius: BorderRadius.circular(12),
                       ),
                       padding:
-                          EdgeInsets.symmetric(horizontal: 14, vertical: 24),
+                          EdgeInsets.symmetric(horizontal: 14, vertical: 20),
                       child: Text(
                         'GET STARTED',
                         style: GoogleFonts.poppins(
@@ -87,32 +84,33 @@ class _HomePageState extends State<HomePage> {
                       child: Text.rich(
                         TextSpan(
                           text: 'Create by ',
-                          style: TextStyle(fontSize: 12),
+                          style: GoogleFonts.galada(fontSize: 12),
                           children: [
                             TextSpan(
-                                text: 'Joshua',
-                                style: GoogleFonts.sanchez(
-                                  color: Colors.white54,
-                                  decoration: TextDecoration.underline,
-                                ),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {
-                                    html.window.open(
-                                        'https://github.com/Josh-cyber09',
-                                        'Github');
-                                  }),
-                            TextSpan(text: '  and  '),
-                            TextSpan(
-                              text: 'Samuel',
-                              style: GoogleFonts.sanchez(
-                                color: Colors.white70,
+                              text: 'Joshua',
+                              style: TextStyle(
+                                letterSpacing: 1.0,
+                                color: Colors.white,
                                 decoration: TextDecoration.underline,
+                                decorationStyle: TextDecorationStyle.wavy,
                               ),
                               recognizer: TapGestureRecognizer()
                                 ..onTap = () {
-                                  html.window.open(
-                                      'https://github.com/techwithsam',
-                                      'Github');
+                                  launch('https://github.com/Josh-cyber09');
+                                },
+                            ),
+                            TextSpan(text: '  and  '),
+                            TextSpan(
+                              text: 'Samuel!',
+                              style: TextStyle(
+                                color: Colors.white,
+                                letterSpacing: 1.0,
+                                decoration: TextDecoration.underline,
+                                decorationStyle: TextDecorationStyle.wavy,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  launch('https://github.com/techwithsam');
                                 },
                             ),
                           ],
@@ -120,7 +118,7 @@ class _HomePageState extends State<HomePage> {
                         textAlign: TextAlign.center,
                       ),
                     ),
-                    SizedBox(height: 30)
+                    SizedBox(height: 10)
                   ],
                 ),
               ),
--- login_ui/lib/screens/sign_in.dart
+++ login_ui/lib/screens/sign_in.dart
@@ -12,151 +12,146 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                AppImages.secondPage,
-              ),
-              fit: BoxFit.fill,
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppImages.secondPage),
+            fit: BoxFit.cover,
           ),
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 30, bottom: 30),
-              child: Card(
-                elevation: 1,
-                color: Color(0xFF251734).withOpacity(1.0),
-                margin: EdgeInsets.only(left: 32, right: 32),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(32)),
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(AppImages.secondBackground),
-                      fit: BoxFit.fill,
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 25, bottom: 25),
+            child: Card(
+              elevation: 1,
+              color: Color(0xFF251734).withOpacity(1.0),
+              margin: EdgeInsets.only(left: 32, right: 32),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(23),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(AppImages.secondBackground),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                height: MediaQuery.of(context).size.height,
+                width: 320,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 120, bottom: 0, left: 20, right: 20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey[50],
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        padding: EdgeInsets.fromLTRB(
+                          20,
+                          5,
+                          20,
+                          5,
+                        ),
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none,
+                                enabledBorder: InputBorder.none,
+                                errorBorder: InputBorder.none,
+                                disabledBorder: InputBorder.none,
+                                hintText: 'Email',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                  height: MediaQuery.of(context).size.height,
-                  width: 320,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            top: 120, bottom: 0, left: 20, right: 20),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.blueGrey[50],
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          padding: EdgeInsets.fromLTRB(
-                            20,
-                            5,
-                            20,
-                            5,
-                          ),
-                          child: Column(
-                            children: [
-                              TextFormField(
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  focusedBorder: InputBorder.none,
-                                  enabledBorder: InputBorder.none,
-                                  errorBorder: InputBorder.none,
-                                  disabledBorder: InputBorder.none,
-                                  hintText: 'Email',
-                                ),
-                              ),
-                            ],
-                          ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey[50],
+                          borderRadius: BorderRadius.circular(25),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.blueGrey[50],
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                          padding: EdgeInsets.fromLTRB(
-                            20,
-                            5,
-                            20,
-                            5,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              TextFormField(
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  focusedBorder: InputBorder.none,
-                                  enabledBorder: InputBorder.none,
-                                  errorBorder: InputBorder.none,
-                                  disabledBorder: InputBorder.none,
-                                  hintText: 'Password',
-                                ),
-                              ),
-                            ],
-                          ),
+                        padding: EdgeInsets.fromLTRB(
+                          20,
+                          5,
+                          20,
+                          5,
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(50, 30, 50, 60),
-                        child: MaterialButton(
-                          onPressed: () {},
-                          color: Colors.blue[900],
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Sign In',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 15,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        child: Text('Forget Password?',
-                            style: TextStyle(color: Colors.blue[900])),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
-                        child: Row(
+                        child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            socialIcon(),
-                            googleIcon(),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                focusedBorder: InputBorder.none,
+                                enabledBorder: InputBorder.none,
+                                errorBorder: InputBorder.none,
+                                disabledBorder: InputBorder.none,
+                                hintText: 'Password',
+                              ),
+                            ),
                           ],
                         ),
                       ),
-                      Container(
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(50, 30, 50, 60),
+                      child: MaterialButton(
+                        onPressed: () {},
+                        color: Colors.blue[900],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text("Don't have an account? ",
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                )),
-                            Text('Sign Up',
-                                style: TextStyle(color: Colors.blue[900])),
+                            Text(
+                              'Sign In',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
                           ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Container(
+                      child: Text('Forget Password?',
+                          style: TextStyle(color: Colors.blue[900])),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          socialIcon(),
+                          googleIcon(),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Don't have an account? ",
+                              style: TextStyle(
+                                color: Colors.grey,
+                              )),
+                          Text('Sign Up',
+                              style: TextStyle(color: Colors.blue[900])),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
